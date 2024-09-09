503b90ede8c078a14711e89c04b42d71679ecd4f
Kristin Andrews <kbrandtsojo@gmail.com>
2024-09-09 17:47:59 -0600

    initial commit

---
+++ todo.js
@@ -0,0 +1,36 @@
+todos = [
+    {
+        name: "wash clothes",
+        status: false,
+        id: 1,
+        category: "chores",
+    }, 
+    {
+        name: "do dishes",
+        status: false,
+        id: 2,
+        category: "chores",
+    },
+    {
+        name: "email quote",
+        status: false,
+        id: 3,
+        category: "work",
+    }
+    
+    ]
+    console.log(todos)
+    
+    
+     
+    ///
+    
+    // function addTodo (newItem){
+    // const todo = {
+    
+    // }
+    
+    // todos.append(todo)
+    // }
+    
+      
