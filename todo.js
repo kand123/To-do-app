6216958eeca3018e6ac63060d3bb2947a5e1e744
Kristin Andrews <kbrandtsojo@gmail.com>
2024-09-19 21:39:03 -0600

    display todos

--- todo.js
+++ todo.js
@@ -1,29 +1,40 @@
+//Todo app
+
+//Basic starting data model
+
 todos = [
     {
         name: "wash clothes",
         status: false,
         id: 1,
         category: "chores",
+        date: ''
     }, 
     {
         name: "do dishes",
         status: false,
         id: 2,
         category: "chores",
+        date: ''
     },
     {
         name: "email quote",
         status: false,
         id: 3,
         category: "work",
+        date: ''
     }
     
     ]
-    console.log(todos)
-    
-    
+
+    // Display todos 
+     todos.map((todo) => {
+         let item = document.createElement('li');
+        item.innerText = todo.name
+        document.querySelector('ul').appendChild(item)
+         })
      
-    ///
+   
     
     // function addTodo (newItem){
     // const todo = {
